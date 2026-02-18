6986623f4fc1d0a5b956e258458c787e9e898345
melatoninastudio <domininsite@gmail.com>
2026-02-18 21:21:07 +0100

    Initial melatonina instance

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-white text-black">
+      <h1 className="text-4xl font-bold">
+        SITO IN COSTRUZIONE 2
+      </h1>
+    </main>
+  );
+}
+
