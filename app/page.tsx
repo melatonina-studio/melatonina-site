9fdc8328c1eaed6d1edf51772faaa178d97cdf38
melatoninastudio <domininsite@gmail.com>
2026-02-18 22:58:01 +0100

    ...

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-white text-black">
       <h1 className="text-4xl font-bold">
-        SITO IN COSTRUZIONE 2
+        MELATONINA STUDIO 1.0
       </h1>
     </main>
   );
