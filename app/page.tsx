5e4aab8bcacc8c6e01191b05bcc1ef5a5e082c20
melatoninastudio <domininsite@gmail.com>
2026-02-19 00:39:49 +0100

    test vercel link

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-white text-black">
       <h1 className="text-4xl font-bold">
-        MELATONINA STUDIO 1.0.15
+        MELATONINA STUDIO 1.0.16
       </h1>
     </main>
   );
